2f0a56f4705314194a093de63c40cf7e82a7b98b
SabarinathanE <sabarielangovan13@gmail.com>
2025-06-30 12:08:41 +0530

    Testimonials

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -16,7 +16,7 @@ function Testimonials() {
         if(tx > -50){
             tx -= 25
         }
-        slider.current.style.transform = `translateX(${tx}%)`
+        slider.current.style.transform = `translateX(${tx}%)` 
     }
 
     const sliderBackward = () => {
